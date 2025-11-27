00318de32f4b23e9a279740256824cc4043a5443
Kharn27 <l_lcypherl_l@hotmail.com>
2025-11-27 15:16:30 +0100

    Repair Build

--- firmware/src/drivers/display_driver.cpp
+++ firmware/src/drivers/display_driver.cpp
@@ -17,9 +17,10 @@ static TFT_eSPI tft = TFT_eSPI();
 #define LV_VER_RES_MAX 240
 #endif
 
-#define DRAW_BUF_LINES 40
+#define DRAW_BUF_LINES 10
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t draw_buf_1[LV_HOR_RES_MAX * DRAW_BUF_LINES];
+static lv_color_t* draw_buf_1 = NULL;
+static bool draw_buf_in_psram = false;
 static lv_disp_t* g_disp = NULL;
 static lv_indev_t* g_indev_touch = NULL;
 
